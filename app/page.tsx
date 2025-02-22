7c587f88af7c35ce683719a6243c6729b076909b
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-02-23 01:48:02 +0530

    Projects Sections is Introduced and using a acertnity comonensts which is a 3d Pin Components and adjustemnts of the floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Grid from "@/Components/Grid";
 import Hero from "@/Components/Hero";
+import RecentsProjects from "@/Components/RecentsProjects";
 import { FloatingNav } from "@/Components/UI/FloatingNavbar";
+import { navItems } from "@/Data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -9,15 +11,11 @@ export default function Home() {
     <main className="relative bg-black-100 text-white  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
         <FloatingNav
-          navItems={[
-            { name: "Home", link: "/" , icon:<FaHome/> },
-            { name: "About", link: "/about" , icon:<FaHome/> },
-            { name: "Projects", link: "/projects" , icon:<FaHome/> },
-            { name: "Contact", link: "/contact" , icon:<FaHome/> },
-          ]}
+          navItems={navItems}
         />
         <Hero />
         <Grid/>
+        <RecentsProjects/>
       </div>
     </main>
   );
